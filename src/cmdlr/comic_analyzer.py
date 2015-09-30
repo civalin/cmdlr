500b844ae8e79d846a1147993d64f51318a7d151
Civa Lin <larina.wf@gmail.com>
2015-09-30 16:40:58 +0800

    comicdb.py and comic_analyzer.py complete basically

--- src/cmdlr/comic_analyzer.py
+++ src/cmdlr/comic_analyzer.py
@@ -1,40 +1,43 @@
 import abc
 
 
+class ComicAnalyzerException(Exception):
+    pass
+
+
 class ComicAnalyzer(metaclass=abc.ABCMeta):
     '''Base class of all comic analyzer'''
 
-    @classmethod
     @abc.abstractmethod
-    def url_to_comic_id(cls, comic_entry_url):
+    def url_to_comic_id(self, comic_entry_url):
         '''
             Convert comic_entry_url to comic_id.
             If convert success return a str format url, else return None.
         '''
 
-    @classmethod
     @abc.abstractmethod
-    def comic_id_to_url(cls, comic_id):
+    def comic_id_to_url(self, comic_id):
         '''
             Convert comic_id to comic_entry_url
+            If convert success return a str format comic_id, else return None.
         '''
 
-    @classmethod
     @abc.abstractmethod
-    def get_comic_info(cls, comic_id):
+    def get_comic_info(self, comic_id):
         '''
-            Get comic info from the comic_entry_url
+            Get comic info from the internet
+            The return data will be saved into user's comic_db
 
             return:
                 {
                     comic_id: <comic_id>,
                     title: <comic_title>,
                     desc: <comic_desc>,
+                    extra_data: {...},
                     volumes: [
                         {
-                            'volume_id': <volume_number>,
+                            'volume_id': <volume_id>,  # e.g., '16', '045'
                             'name': <volume_name>,
-                            'volume_entry_url': <url>,
                         },
                         {...},
                         ...
@@ -42,13 +45,22 @@ class ComicAnalyzer(metaclass=abc.ABCMeta):
                 }
         '''
 
-    @classmethod
     @abc.abstractmethod
-    def get_volume_pages(cls, volume_entry_url):
+    def get_volume_pages(self, comic_id, volume_id, extra_data):
         '''
+            Get images url for future download.
+
+            args:
+                comic_id:
+                    which comic you want to analysis
+                volume_id:
+                    which volume you want to analysis
+                extra_data:
+                    the comic extradata create by self.get_comic_info()
+
             yield:
                 {
                     'url': <image_url>,
-                    'local_filename': <local_filename>,
+                    'local_filename': <local_filename>,  # e.g., 12.jpg
                 }
         '''
--- src/cmdlr/comicdb.py
+++ src/cmdlr/comicdb.py
@@ -4,9 +4,14 @@ import datetime as DT
 import pickle
 
 
+_DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S'
+
+
 def extend_sqlite3_datatype():
     sqlite3.register_adapter(DT.datetime, pickle.dumps)
     sqlite3.register_converter('DATETIME', pickle.loads)
+    sqlite3.register_adapter(dict, pickle.dumps)
+    sqlite3.register_converter('DICT', pickle.loads)
 
 
 extend_sqlite3_datatype()
@@ -23,6 +28,17 @@ class ComicDB():
                 self.conn.execute('PRAGMA user_version = {};'.format(
                     int(version)))
 
+            def insert_option(option, value):
+                '''
+                    insert a option and its value.
+                    Both 2 args must be str type or None.
+                '''
+                self.conn.execute(
+                    'INSERT INTO "options" (option, value)'
+                    'VALUES (:option, :value)',
+                    {'option': option, 'value': value}
+                )
+
             def from0to1():
                 self.conn.execute(
                     'CREATE TABLE comics ('           # 已訂閱的漫畫
@@ -30,6 +46,7 @@ class ComicDB():
                     'title TEXT NOT NULL,'            # e.g., 海賊王
                     'desc TEXT NOT NULL,'             # e.g., 關於海賊的漫畫
                     'created_time DATETIME NOT NULL,'
+                    'extra_data DICT'    # extra data package
                     ');'
                 )
                 self.conn.execute(
@@ -42,11 +59,13 @@ class ComicDB():
                     ');'
                 )
                 self.conn.execute(
-                    'CREATE TABLE metadata ('
-                    'output_dir TEXT NOT NULL,'
-                    'last_refresh_time DATETIME,'
+                    'CREATE TABLE options ('
+                    'option TEXT PRIMARY KEY NOT NULL,'
+                    'value TEXT,'
                     ');'
                 )
+                insert_option('output_dir', os.path.expanduser('~/comics'))
+                insert_option('last_refresh_time', None)
                 set_db_version(1)
 
             db_version = get_db_version()
@@ -56,23 +75,6 @@ class ComicDB():
 
             self.conn.commit()
 
-        def metadata_init():
-            def is_metadata_row_exists():
-                exist = self.conn.execute(
-                    'SELECT COUNT(*) FROM "metadata"').fetchone()[0]
-                return bool(exist)
-
-            def insert_default_metadata():
-                default_output_dir = os.path.expanduser('~/comics')
-                self.conn.execute(
-                    'INSERT INTO "metadata" ("output_dir")'
-                    'VALUES (:output_dir)',
-                    {'output_dir': default_output_dir}
-                )
-
-            if not is_metadata_row_exists():
-                insert_default_metadata()
-
         self.conn = sqlite3.connect(dbpath,
                                     detect_types=sqlite3.PARSE_DECLTYPES)
         self.conn.row_factory = sqlite3.Row
@@ -80,27 +82,57 @@ class ComicDB():
         self.migrate()
         self.__metadata_init()
 
+    def __get_option(self, option):
+        '''
+            return the option value
+        '''
+        return self.conn.execute(
+            'SELECT "value" FROM "options" where option = :option',
+            {'option': option}).fetchone()['value']
+
+    def __set_option(self, option, value):
+        '''
+            set the option value, the value must be str or None.
+        '''
+        self.conn.execute(
+            'UPDATE "options" SET "value" = :value'
+            ' WHERE option = :option',
+            {'value': value, 'option': option})
+
     @property
     def output_dir(self):
-        return self.conn.execute(
-            'SELECT "output_dir" FROM "metadata"').fetchone()['output_dir']
+        return self.__get_option('output_dir')
 
     @output_dir.setter
     def output_dir(self, output_dir):
-        self.conn.execute(
-            'UPDATE "metadata" SET "output_dir" = :output_dir',
-            {'output_dir': output_dir})
+        self.__set_option('output_dir', output_dir)
 
     @property
     def last_refresh_time(self):
-        return self.conn.execute('SELECT "last_refresh_time" FROM "metadata"'
-                                 ).fetchone()['last_refresh_time']
+        '''
+            return None or datetime format
+        '''
+        lrt_strformat = self.__get_option('last_refresh_time')
+        if lrt_strformat is None:
+            return None
+        else:
+            last_refresh_time = DT.datetime.strptime(
+                lrt_strformat, _DATETIME_FORMAT)
+            return last_refresh_time
 
     @last_refresh_time.setter
     def last_refresh_time(self, last_refresh_time):
-        self.conn.execute(
-            'UPDATE "metadata" SET "last_refresh_time" = :last_refresh_time',
-            {'last_refresh_time': last_refresh_time})
+        '''
+            args:
+                last_refresh_time:
+                    must be datetime format
+        '''
+        if last_refresh_time is None:
+            self.__set_option(None)
+        else:
+            lrt_strformat = DT.datetime.strftime(
+                last_refresh_time, '%Y-%m-%dT%H:%M:%S')
+            self.__set_option('last_refresh_time', lrt_strformat)
 
     def upsert_comic(self, comic_info):
         '''
@@ -183,8 +215,33 @@ class ComicDB():
         return self.conn.execute(
             'SELECT * FROM comics INNER JOIN volumes'
             ' ON comics.comic_id = volumes.comic_id'
-            ' WHERE is_downloaded = 0'
+            ' WHERE volumes.is_downloaded = 0'
             ' ORDER BY comic_id').fetchall()
 
-    def get_all_comics():
-        pass
+    def get_all_comics(self):
+        return self.conn.execute(
+            'SELECT * FROM comics'
+            ' ORDER BY created_time').fetchall()
+
+    def get_comic_volumes_status(self, comic_id):
+        '''
+            Use for UI display.
+        '''
+        volumes = self.conn.execute(
+            'SELECT * FROM volumes'
+            ' WHERE comic_id = :comic_id',
+            {'comic_id': comic_id}).fetchall()
+        data = {
+            'total': len(volumes),
+            'downloaded_count': 0,
+            'no_downloaded_count': 0,
+            'no_downloaded_names': [],
+            }
+        for volume in volumes:
+            if(volume['is_downloaded']):
+                data['downloaded_count'] = data['downloaded_count'] + 1
+            else:
+                data['no_downloaded_count'] = data['no_downloaded_count'] + 1
+                data['no_downloaded_names'].append(volume['name'])
+
+        return data
