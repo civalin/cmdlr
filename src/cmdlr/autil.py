503168d4c1db8312134d7782a8278dfa3c179260
visig <visig@protonmail.ch>
2018-10-26 20:33:15 +0800

    add helper function autil.fetch()

--- src/cmdlr/autil.py
+++ src/cmdlr/autil.py
@@ -5,11 +5,14 @@ import json
 import tempfile
 import subprocess
 from collections import namedtuple
+from urllib.parse import urljoin
+
+from bs4 import BeautifulSoup
 
 from .exception import ExternalDependencyNotFound
 
 
-JSResult = namedtuple('JSResult', ['eval', 'env'])
+_JSResult = namedtuple('JSResult', ['eval', 'env'])
 
 
 def run_in_nodejs(js):
@@ -46,4 +49,23 @@ def run_in_nodejs(js):
             f.name,
         ])
 
-    return JSResult(**json.loads(ret_value.decode()))
+    return _JSResult(**json.loads(ret_value.decode()))
+
+
+_FetchResult = namedtuple('FetchResult', ['soup', 'get_abspath'])
+
+
+async def fetch(url, request, encoding='utf8', **req_kwargs):
+    """Get BeautifulSoup from remote url."""
+    async with request(url, **req_kwargs) as resp:
+        binary = await resp.read()
+        text = binary.decode(encoding, errors='ignore')
+        soup = BeautifulSoup(text, 'lxml')
+
+        base_url = str(resp.url)
+
+    def get_abspath(url):
+        return urljoin(base_url, url)
+
+    return _FetchResult(soup=soup,
+                        get_abspath=get_abspath)
