247d64c1d667b4e7cd6237e53f014bf8b255dfef
visig <visig@protonmail.ch>
2018-10-28 08:16:53 +0800

    fix: autil.fetch().absurl function should consider <base> tag

--- src/cmdlr/autil.py
+++ src/cmdlr/autil.py
@@ -63,10 +63,16 @@ async def fetch(url, request, encoding='utf8', **req_kwargs):
     """Get BeautifulSoup from remote url."""
     async with request(url, **req_kwargs) as resp:
         binary = await resp.read()
-        text = binary.decode(encoding, errors='ignore')
-        soup = BeautifulSoup(text, 'lxml')
-
         base_url = str(resp.url)
+
+    text = binary.decode(encoding, errors='ignore')
+    soup = BeautifulSoup(text, 'lxml')
+
+    base_tag = soup.select_one('html > head > base[href]')
+
+    if base_tag:
+        base_href = base_tag.get('href')
+        base_url = urljoin(base_url, base_href)
 
     def absurl(url):
         return urljoin(base_url, url)
